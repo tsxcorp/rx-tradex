97032dc92022be5c8ddfe8b9399a1c39cf046337
readyagency <ready.agency.vn@gmail.com>
2024-06-06 17:36:46 +0700

    Update date.vietnam.tsx

--- app/components/date.vietnam.tsx
+++ app/components/date.vietnam.tsx
@@ -1,7 +1,7 @@
 import { parseISO, format } from 'date-fns';
-import vi from 'date-fns/locale/vi';
+import { vi } from 'date-fns/locale';
 
 export default function Date({ dateString } : any) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'dd/MM/yyyy', {locale: vi})}</time>;
+  return <time dateTime={dateString}>{format(date, 'dd/MM/yyyy', { locale: vi })}</time>;
 }
